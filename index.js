f2a06a03f4b0bf86e2023636886712b8701aab49
Kevin-MYJ <wengehuastudy2016@gmail.com>
2021-03-23 17:06:09 -0700

    some database

--- index.js
+++ index.js
@@ -20,3 +20,55 @@ app.set('view engine', 'ejs');
 server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.get('/', (req, res) => res.render('pages/login'));
+
+/* create table users(
+  username varchar(10) not null,
+  password varchar(20) not null,
+  email varchar(30) not null,
+  type varchar(10) not null,
+  primary key(username)
+);*/
+
+/* create table usersdata(
+  userid varchar(10),
+  difficulty varchar(10) not null,
+  time varchar(10) not null,
+  score int not null,
+  datetime date not null,
+  foreign key (user) references users(username)
+);*/
+
+/*insert into users values(
+  kevinMa,
+  123456,
+  wengehuastudy2016@gmail.com,
+  admin
+);*/
+
+/*insert into users values(
+  dezhengZhong,
+  123456,
+  zdz14155023@gmail.com,
+  admin
+);*/
+
+/*insert into users values(
+  meikoMi,
+  123456,
+  meiko19617336@gmail.com,
+  admin
+);*/
+
+/*insert into users values(
+  sn3an,
+  123456,
+  sunzhijie1998@gmail.com,
+  admin
+);*/
+
+/*insert into users values(
+  inhwamo,
+  123456,
+  inhwa2000@gmail.com,
+  admin
+);*/
